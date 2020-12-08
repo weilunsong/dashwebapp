b675e2e0f328778f29411675fa69f2439d71e0f4
Weilun <36088971+weilunsong@users.noreply.github.com>
2020-12-08 16:29:36 -0500

    Add label

--- apps/Client_Name.py
+++ apps/Client_Name.py
@@ -12,7 +12,7 @@ PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
 
 df3 = pd.read_csv(DATA_PATH.joinpath('Build.csv'))
-df3 = df3[['Client_Name', 'Completed', 'Brand']]
+df3 = df3[['Client_Name', 'SectionName', 'Completed', 'Brand']]
 Completed = []
 for i in df3.Completed:
     if i == True:
@@ -34,8 +34,19 @@ df_brand['Incomplete_Rate'] = df_brand['Incomplete_Rate'].astype(float).map("{:.
 
 dfb = df_brand
 
-sort_label = sorted(dfb.Brand.unique())
-sort_label.insert(0, 'All')
+df_sc = pd.DataFrame()
+for i in list(df3.Brand.unique()):
+    df = df3[df3['Brand'] == i]
+    df = 1 - df.groupby(['SectionName']).mean().round(2)
+    df.reset_index(inplace=True)
+    df['Brand'] = i
+    df = df.rename({'Completed': 'Incomplete_Rate'}, axis=1)
+    df_sc = df_sc.append(df)
+
+df_sc['Incomplete_Rate'] = df_sc['Incomplete_Rate'].astype(float).map("{:.2%}".format)
+
+sort_label_client = sorted(dfb.Brand.unique())
+sort_label_client.insert(0, 'All')
 
 layout = html.Div([
     html.H1('', style={"textAlign": "center"}),
@@ -44,8 +55,15 @@ layout = html.Div([
         dcc.Dropdown(id='Client_Name_select',
                      persistence=True,
                      persistence_type='session',
-                     options=[{'label': i, 'value': i} for i in sort_label],
+                     options=[{'label': i, 'value': i} for i in sort_label_client],
                      value='All',
+                     style={'width': '500px'})]),
+    html.Div([
+        dcc.Dropdown(id='Section_Name_select',
+                     persistence=True,
+                     persistence_type='session',
+                     options=[{'label': i, 'value': i} for i in ['Client Name', 'Section Name']],
+                     value='Client Name',
                      style={'width': '500px'})
     ]),
     dcc.Graph('Client_Name_graph', config={}),
@@ -53,37 +71,58 @@ layout = html.Div([
 ])
 
 
-@app.callback(Output('Client_Name_graph', 'figure'), [Input('Client_Name_select', 'value')])
-def update_graph(name):
-    if name == 'All':
+@app.callback(Output('Client_Name_graph', 'figure'), [Input('Client_Name_select', 'value')],
+              [Input('Section_Name_select', 'value')])
+def update_client(client, section):
+    if client == 'All':
         df = 1 - df3.groupby(['Brand']).mean().round(2)
         df.reset_index(inplace=True)
         df['Completed'] = df['Completed'].astype(float).map("{:.2%}".format)
-        fig = go.Figure(data=[go.Table(header=dict(values=['Brand', 'Incomplete_Rate']),
-                                       cells=dict(values=[df.Brand,
-                                                          df.Completed]))
-                              ])
-    else:
-        fig = go.Figure(data=[go.Table(header=dict(values=['Client_Name', 'Incomplete_Rate']),
-                                       cells=dict(values=[dfb[dfb.Brand == name].Client_Name,
-                                                          dfb[dfb.Brand == name].Incomplete_Rate]))
-                              ])
+        fig1 = go.Figure(data=[go.Table(header=dict(values=['Brand', 'Incomplete_Rate']),
+                                        cells=dict(values=[df.Brand,
+                                                           df.Completed]))])
+        return fig1
+    elif section == 'Client Name':
+        fig2 = go.Figure(data=[go.Table(header=dict(values=['Client_Name', 'Incomplete_Rate']),
+                                        cells=dict(values=[dfb[dfb.Brand == client].Client_Name,
+                                                           dfb[dfb.Brand == client].Incomplete_Rate]))])
+        return fig2
+    elif section == 'Section Name':
+        fig3 = go.Figure(data=[go.Table(header=dict(values=['Section_Name', 'Incomplete_Rate']),
+                                        cells=dict(values=[df_sc[df_sc.Brand == client].SectionName,
+                                                           df_sc[df_sc.Brand == client].Incomplete_Rate]))])
+        return fig3
 
-    return fig
 
-
-@app.callback(Output('Client_Name_pie', 'figure'), [Input('Client_Name_select', 'value')])
-def update_pie(name):
-    if name == 'All':
+@app.callback(Output('Client_Name_pie', 'figure'),
+              [Input('Client_Name_select', 'value')], [Input('Section_Name_select', 'value')])
+def update_pie(client, section):
+    if client == 'All':
         df_pie = df3
-    else:
-        df_pie = df3[df3['Brand'] == name]
-    values = [len(df_pie[df_pie.Completed == 1]), len(df_pie[df_pie.Completed == 0])]
-    names = ['Completed', 'Incompleted']
-    fig = px.pie(values=values,
-                 names=names,
-                 title='Completed rate',
-                 color_discrete_sequence=["lightsalmon", "cornflowerblue"])
-    fig.update_traces(textinfo='percent+label')
+        values = [len(df_pie[df_pie.Completed == 1]), len(df_pie[df_pie.Completed == 0])]
+        names = ['Completed', 'Incompleted']
+        figp = px.pie(values=values,
+                      names=names,
+                      title='Completed rate',
+                      color_discrete_sequence=["lightsalmon", "cornflowerblue"])
+        figp.update_traces(textinfo='percent+label')
+        return figp
+    elif section == 'Client Name':
+        fig1 = px.bar(dfb[dfb.Brand == client],
+                      x='Client_Name',
+                      y='Incomplete_Rate',
+                      text=dfb[dfb.Brand == client].Incomplete_Rate)
+        fig1.update_traces(textposition='outside')
+        for data in fig1.data:
+            data['width'] = 0.15
+        return fig1
 
-    return fig
+    elif section == 'Section Name':
+        fig2 = px.bar(df_sc[df_sc.Brand == client],
+                      x='SectionName',
+                      y='Incomplete_Rate',
+                      text=df_sc[df_sc.Brand == client].Incomplete_Rate)
+        fig2.update_traces(textposition='outside')
+        for data in fig2.data:
+            data['width'] = 0.15
+        return fig2
--- apps/OEM.py
+++ apps/OEM.py
@@ -12,7 +12,7 @@ PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
 
 df1 = pd.read_csv(DATA_PATH.joinpath('Tier3.csv'))
-df1 = df1[['OEM', 'Completed', 'Project_Name']]
+df1 = df1[['OEM', 'SectionName', 'Completed', 'Project_Name']]
 Completed = []
 for i in df1.Completed:
     if i == True:
@@ -31,8 +31,18 @@ for i in list(df1.Project_Name.unique()):
     df_oem = df_oem.append(df)
 
 df_oem['Incomplete_Rate'] = df_oem['Incomplete_Rate'].astype(float).map("{:.2%}".format)
-
 dfo = df_oem
+
+dfo_sc = pd.DataFrame()
+for i in list(df1.Project_Name.unique()):
+    df = df1[df1['Project_Name'] == i]
+    df = 1 - df.groupby(['SectionName']).mean().round(2)
+    df.reset_index(inplace=True)
+    df['Project_Name'] = i
+    df = df.rename({'Completed': 'Incomplete_Rate'}, axis=1)
+    dfo_sc = dfo_sc.append(df)
+
+dfo_sc['Incomplete_Rate'] = dfo_sc['Incomplete_Rate'].astype(float).map("{:.2%}".format)
 
 layout = html.Div([
     html.H1('', style={"textAlign": "center"}),
@@ -45,28 +55,55 @@ layout = html.Div([
                      value='December National Incentives',
                      style={'width': '500px'})
     ]),
+    html.Div([
+        dcc.Dropdown(id='Section_Name_select',
+                     persistence=True,
+                     persistence_type='session',
+                     options=[{'label': i, 'value': i} for i in ['OEM', 'Section Name']],
+                     value='OEM',
+                     style={'width': '500px'})
+    ]),
     dcc.Graph('OEM_graph', config={}),
     dcc.Graph('OEM_pie', config={})
 ])
 
 
-@app.callback(Output('OEM_graph', 'figure'), [Input('OEM_select', 'value')])
-def update_graph(name):
-    fig = go.Figure(data=[go.Table(header=dict(values=['OEM', 'Incomplete_Rate']),
-                                   cells=dict(values=[dfo[dfo.Project_Name == name].OEM,
-                                                      dfo[dfo.Project_Name == name].Incomplete_Rate]))
-                          ])
+@app.callback(Output('OEM_graph', 'figure'),
+              [Input('OEM_select', 'value')], [Input('Section_Name_select', 'value')])
+def update_graph(oem, section):
+    if section == 'OEM':
+        fig = go.Figure(data=[go.Table(header=dict(values=['OEM', 'Incomplete_Rate']),
+                                       cells=dict(values=[dfo[dfo.Project_Name == oem].OEM,
+                                                          dfo[dfo.Project_Name == oem].Incomplete_Rate]))
+                              ])
+    else:
+        fig = go.Figure(data=[go.Table(header=dict(values=['Section Name', 'Incomplete_Rate']),
+                                       cells=dict(values=[dfo_sc[dfo_sc.Project_Name == oem].SectionName,
+                                                          dfo_sc[dfo_sc.Project_Name == oem].Incomplete_Rate]))
+                              ])
+
     return fig
 
 
-@app.callback(Output('OEM_pie', 'figure'), [Input('OEM_select', 'value')])
-def update_pie(name):
-    df_pie = df1[df1['Project_Name'] == name]
-    values = [len(df_pie[df_pie.Completed == 1]), len(df_pie[df_pie.Completed == 0])]
-    names = ['Completed', 'Incompleted']
-    fig = px.pie(values=values,
-                 names=names,
-                 title='Completed rate',
-                 color_discrete_sequence=["lightsalmon", "cornflowerblue"])
-    fig.update_traces(textinfo='percent+label')
+@app.callback(Output('OEM_pie', 'figure'),
+              [Input('OEM_select', 'value')], [Input('Section_Name_select', 'value')])
+def update_pie(name, section):
+    if section == 'OEM':
+        df_pie = df1[df1['Project_Name'] == name]
+        values = [len(df_pie[df_pie.Completed == 1]), len(df_pie[df_pie.Completed == 0])]
+        names = ['Completed', 'Incompleted']
+        fig = px.pie(values=values,
+                     names=names,
+                     title='Completed rate',
+                     color_discrete_sequence=["lightsalmon", "cornflowerblue"])
+        fig.update_traces(textinfo='percent+label')
+    else:
+        fig = px.bar(dfo_sc[dfo_sc.Project_Name == name],
+                     x='SectionName',
+                     y='Incomplete_Rate',
+                     text=dfo_sc[dfo_sc.Project_Name == name].Incomplete_Rate)
+        fig.update_traces(textposition='outside')
+        for data in fig.data:
+            data['width'] = 0.15
+
     return fig
